8bf9165dde6ca4a114cdb7ad96d022e279828479
Gloria Fernandez <gloriafer18@gmail.com>
2014-09-21 18:18:40 -0500

    My Assigment

---
+++ READMEgloriafer18.md
@@ -0,0 +1,8 @@
+### How to start run the Script
+
+1. Run the Script
+2. Create a Matrix 
+a <- makeCacheMatrix( matrix(c(1,2,3,12,13,14), nrow = 3, ncol = 3) );
+3. Then run summary(a);
+4. Print the matrix values a$getMatrix();
+5. Get the inverse of a cacheSolve(a)
--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,54 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+    # initialize caches with NULL
+    caches <- NULL
+  
+    # store a matrix
+    setMatrix <- function(newValue) {
+      x <<- newValue
+      # flush the cache
+      caches <<- NULL
+    }
+  
+    # returns the matrix
+    getMatrix <- function() {
+      x
+    }
+  
+    # cache the argument that was given
+    cacheInverse <- function(solve) {
+      caches <<- solve
+    }
+  
+    # get the cached value
+    getInverse <- function() {
+      caches
+    }
+  
+    # return a list. Each named element of the list is a function
+    list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)  
+
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    # get the cached value
+    inverse <- x$getInverse()
+    # if a cached value exists return it
+    if(!is.null(inverse)) {
+      message("getting cached info")
+      return(inverse)
+    }
+    # otherwise get the matrix, caclulate the inverse and store it in
+    # the cache
+    data <- x$getMatrix()
+    inverse <- solve(data)
+    x$cacheInverse(inverse)
+    
+    # return the inverse
+    inverse
 }
